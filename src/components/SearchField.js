fb8a8fd129c00ab7309d46faf20653d4a9cc86c1
NapatKarm <napat.karm@gmail.com>
2019-07-25 14:02:02 -0400

    Created base files, App.js, SearchField.js, GifCard.js

---
+++ src/components/SearchField.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class SearchField extends Component {
+    constructor(props){
+      super(props);
+    }
+    render(){
+      return (
+        <div>
+        </div>
+      );
+    }
+  }
+
+
+export default SearchField;
