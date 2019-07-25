c6d8c9fd98aede20830174bf24d908254abcba10
NapatKarm <napat.karm@gmail.com>
2019-07-25 14:40:36 -0400

    Added to SearchField (unfinished)

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -4,10 +4,39 @@ import axios from 'axios';
 class SearchField extends Component {
     constructor(props){
       super(props);
+      this.state={
+          data : [],
+          searching: "",
+          searched: ""
+      }
+    }
+    searchChange=(event)=>{
+        this.setState({searching : event.target.value});
+    }
+    searchUp=()=>{
+        axios.get(`http://api.giphy.com/v1/gifs/search?q=${this.state.searching}&api_key=OryF3O6rJEiY1Xxpq0sw1SLruxHwxZOs`)
+        .then(res => {
+          console.log(res.data)
+          this.setState({
+            data : res.data,
+            searched: this.state.searching
+          })
+        })
+        .catch(err => {
+          console.log(err);
+        })
     }
     render(){
       return (
         <div>
+            <form  className = "form">
+                <label>
+                    Search:
+                    <input type="text" name="search" value={this.state.searching} onChange={this.searchChange}/>
+                </label>
+            </form>
+            <button onClick={this.searchUp} class = "button">Submit</button>
+            {this.state.searched}
         </div>
       );
     }
